0485f740e539db83158788506c80b67b955b268e
vefa1997 <vefabaku97@gmail.com>
2023-12-07 16:43:55 +0400

    feature: aside update

--- src/shared/layout/SideMenu/index.jsx
+++ src/shared/layout/SideMenu/index.jsx
@@ -82,10 +82,16 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons';
 import { Link, useLocation } from "react-router-dom";
-import { Typography } from "antd";
+import { Typography, Image } from "antd";
+import Logo from "src/shared/media/img/KB Logo.svg";
 
 
 import { Layout, Menu, Button, theme } from 'antd';
+import {
+  BarChart, Table, Users, User, Success, Share, Briefcase, ClipboardCheck,
+  Building, Edit, Folder
+
+} from 'src/assets/svgs';
 const { Header, Sider, Content } = Layout;
 
 
@@ -94,80 +100,204 @@ const SideMenu = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+  const { pathname } = useLocation()
+
+  console.log(pathname)
+
+  const { SubMenu } = Menu
 
 
   const items = [
     {
       key: '1',
-      icon: <UserOutlined />,
-      label: 'nav 1',
+      icon: <BarChart />,
+      label: 'Statistika',
       url: "/continents"
     },
     {
       key: '2',
-      icon: <VideoCameraOutlined />,
-      label: 'nav 2',
+      icon: <Table />,
+      label: 'Vakansiya statistikalar',
       url: "/customs-offices"
 
     },
     {
       key: '3',
-      icon: <UploadOutlined />,
-      label: 'nav 3',
+      icon: <Users />,
+      label: 'Sistem istifadəçiləri',
+      url: "/test3"
+
+    },
+
+    {
+      key: '4',
+      icon: <User />,
+      label: 'Namizədlər',
       url: "/test4"
 
     },
+
+    {
+      key: '5',
+      icon: <Success />,
+      label: 'Uğur hekayələri',
+      url: "/test5"
+
+    },
+
+
+    {
+      key: 'sub6',
+      icon: <Share />,
+      label: 'Paylaşımlar',
+      children: [
+        {
+          key: '1',
+          label: 'Sorğular',
+          url: "test4",
+          isActive: pathname === "test4",
+        },
+        {
+          key: '2',
+          label: 'Sifarişlər',
+          url: "test5",
+          isActive: pathname === "test5",
+        },
+      ],
+    },
+
+
+    {
+      key: 'sub7',
+      icon: <Briefcase />,
+      label: 'Vakansiyalar',
+      children: [
+        {
+          key: '3',
+          label: 'Sorğular',
+          url: "/about",
+          isActive: pathname === "/about",
+        },
+        {
+          key: '4',
+          label: 'Sifarişlər',
+          url: 'Sifarişlər',
+          isActive: pathname === 'Sifarişlər',
+        },
+      ],
+    },
+
+
+    {
+      key: 'sub8',
+      icon: <Folder />,
+      label: 'Qovluqlar',
+    },
+
+
+    {
+      key: 'sub9',
+      icon: <Edit />,
+      label: 'Redaktə',
+      children: [
+        {
+          key: '5',
+          label: 'Sorğular',
+          // url: Urls.test14,
+          // isActive: pathname === Urls.test14,
+        },
+        {
+          key: '6',
+          label: 'Sifarişlər',
+          // url: Urls.test15,
+          // isActive: pathname === Urls.test15,
+        },
+      ],
+    },
+
+
+    {
+      key: 'sub10',
+      icon: <Building />,
+      label: 'Struktur',
+      children: [
+        {
+          key: '7',
+          label: 'Baş ofis',
+          // url: Urls.test16,
+          // isActive: pathname === Urls.test16,
+        },
+        {
+          key: '8',
+          label: 'Filiallar',
+          // url: Urls.test17,
+          // isActive: pathname === Urls.test17,
+        },
+        {
+          key: '8',
+          label: 'Bölmələr',
+          // url: Urls.test17,
+          // isActive: pathname === Urls.test17,
+        },
+        {
+          key: '8',
+          label: 'Şöbələr',
+          // url: Urls.test17,
+          // isActive: pathname === Urls.test17,
+        },
+        {
+          key: '8',
+          label: 'Vəzifələr',
+          // url: Urls.test17,
+          // isActive: pathname === Urls.test17,
+        },
+      ],
+    },
+
+    {
+      key: 'sub11',
+      icon: <ClipboardCheck />,
+      label: 'Gerchikov',
+      children: [
+        {
+          key: '9',
+          label: 'Sorğular',
+          // url: Urls.test18,
+          // isActive: pathname === Urls.test18,
+        },
+        {
+          key: '10',
+          label: 'Sifarişlər',
+          // url: Urls.test19,
+          // isActive: pathname === Urls.test19,
+        },
+      ],
+    },
+
+
+
   ]
 
-  const { pathname } = useLocation()
 
-  console.log(pathname)
+
+
   return (
+
+
     <Layout>
-      {/* <Sider trigger={null} collapsible collapsed={collapsed}>
+      <Sider width={300} trigger={null} collapsible collapsed={collapsed}>
         <div className="demo-logo-vertical" />
-        <Menu
+        {/* <Menu
           theme="dark"
           mode="inline"
           defaultSelectedKeys={['1']}
    
-        />
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
-          <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          />
-        </Header>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-          }}
-        >
-          Content
-        </Content>
-      </Layout> */}
+        /> */}
 
-
-      <Sider className='kt-aside' width={300} collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-        {/* <Typography className="demo-logo-vertical mb-3">
+        <Typography className="demo-logo-vertical ">
           <Image src={Logo} preview={false} height={50} width={collapsed ? 50 : 100} />
-        </Typography> */}
+        </Typography>
+
         <Menu theme="dark" mode="inline" selectedKeys={[pathname]}>
           {items.map(item => (
             <>
@@ -199,6 +329,35 @@ const SideMenu = () => {
           ))}
         </Menu>
       </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
     </Layout>
   );
 };
